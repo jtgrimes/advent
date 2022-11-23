1b93b56d9b6e3982215646cca5ea9c4cf33e4efa
jtgrimes <jtgrimes@gmail.com>
2022-11-23 12:55:37 -0800

    it's ugly, but it's day 9

---
+++ src/y2015/Day9.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Jtgrimes\Advent\y2015;
+
+use Jtgrimes\Advent\Day;
+use Jtgrimes\Advent\y2015\Support\Permutator;
+
+class Day9 extends Day
+{
+    protected $year = 2015;
+    protected $day = 9;
+
+    public function part1()
+    {
+        $distances = $this->calculateDistances();
+        return collect($distances)->min();
+    }
+
+    public function part2()
+    {
+        $distances = $this->calculateDistances();
+        return collect($distances)->max();
+    }
+
+    private function calculateDistances()
+    {
+        $permuter = new Permutator();
+        $legs = [];
+        $cities = [];
+        $lines = $this->getInputAsArrayOfLines();
+        foreach ($lines as $line) {
+            $parts = explode(' ', $line);
+            // format is CITY to CITY = NUM, so we want parts 0,2,4
+            $legs[$parts[0]][$parts[2]] = (int)$parts[4];
+            $legs[$parts[2]][$parts[0]] = (int)$parts[4];
+
+            if (! in_array($parts[0], $cities)) {
+                $cities[] = $parts[0];
+            }
+            if (! in_array($parts[2], $cities)) {
+                $cities[] = $parts[2];
+            }
+        }
+        print_r($cities);
+        $possibleRoutes = $permuter->permutations($cities);
+        $distances = [];
+        foreach ($possibleRoutes as $route) {
+            $distance = 0;
+            foreach ($route as $i => $city) {
+                if ($i == 0) {
+                    // at first stop, no incoming leg
+                } else {
+                    $distance += $legs[$route[$i - 1]][$city];
+                }
+            }
+            $distances[] = $distance;
+        }
+        return $distances;
+    }
+
+}
