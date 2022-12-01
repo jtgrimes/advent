eab2b38c8adac8e1078b0d37caa8848871ec96b9
jtgrimes <jtgrimes@gmail.com>
2022-12-01 08:07:04 -0800

    2021-day1

---
+++ src/y2021/Day1.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Jtgrimes\Advent\y2021;
+
+class Day1 extends \Jtgrimes\Advent\Day
+{
+
+    public function part1()
+    {
+        $lines = $this->getInputAsArrayOfLines();
+        $increases = 0;
+        foreach ($lines as $i => $line) {
+            if ($i == 0) {
+                // do nothing
+            } else {
+                if ((int)$line > (int)$lines[$i-1]) {
+                    $increases++;
+                }
+            }
+        }
+        return $increases;
+    }
+
+    public function part2()
+    {
+        $lines = $this->getInputAsArrayOfLines();
+        $increases = 0;
+        foreach ($lines as $i => $line) {
+            if ($i < 3) {
+                // do nothing
+            } else {
+                if ($this->window($i, $lines) > $this->window($i-1, $lines)) {
+                    $increases++;
+                }
+            }
+        }
+        return $increases;
+    }
+
+    private function window(int $i, array $lines)
+    {
+        return $lines[$i] + $lines[$i-1] + $lines[$i-2];
+    }
+}
