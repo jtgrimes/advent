a64c5ab939b7903ca667dda7a40b6a4e55fa5c41
jtgrimes <jtgrimes@gmail.com>
2022-12-03 08:18:12 -0800

    2022-day3

---
+++ src/y2022/Day3.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Jtgrimes\Advent\y2022;
+
+class Day3 extends \Jtgrimes\Advent\Day
+{
+
+    public function part1()
+    {
+        $score = 0;
+        $rucks = $this->getInputAsArrayOfLines();
+        foreach ($rucks as $ruck) {
+            $items = str_split(trim($ruck));
+            $secondHalf = array_splice($items, count($items) / 2, count($items), []);
+            // items now only contains the first half ... rename because I'll confuse myself otherwise
+            $firstHalf = $items;
+            $duplicates = array_intersect($firstHalf, $secondHalf);
+            $duplicate = array_values($duplicates)[0];
+            echo ("$duplicate: ".$this->toScore($duplicate)."\n");
+            $score += $this->toScore($duplicate);
+        }
+        return $score;
+    }
+
+    public function part2()
+    {
+        $rucks = collect($this->getInputAsArrayOfLines());
+        return $rucks->chunk(3)->map(function ($group){
+            $first = str_split(trim($group->shift()));
+            $second = str_split(trim($group->shift()));
+            $third = str_split(trim($group->shift()));
+            $duplicates = array_intersect($first, $second, $third);
+            $duplicate = array_values($duplicates)[0];
+            echo ("$duplicate: ".$this->toScore($duplicate)."\n");
+            return $this->toScore($duplicate);
+        })->sum();
+    }
+
+    private function toScore($char)
+    {
+        if (ord($char) < 91) {
+            // upper case
+            return ord($char) - 38; // sets A to 27
+        }
+        return ord($char) - 96;
+    }
+}
