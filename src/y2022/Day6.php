e2bea9bb6a04702dc80a8e87a16b2da68f646b27
jtgrimes <jtgrimes@gmail.com>
2022-12-06 08:10:04 -0800

    2022-day6

---
+++ src/y2022/Day6.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Jtgrimes\Advent\y2022;
+
+use Illuminate\Support\Str;
+use Jtgrimes\Advent\Day;
+
+class Day6 extends Day
+{
+
+    public function part1()
+    {
+        $input = $this->getInputAsString();
+        for ($i = 3; $i < strlen($input); $i++) {
+            if ($this->noDuplicateCharacters(substr($input, $i-3, 4))) {
+                return $i + 1; // Elves use 1-indexed arrays, not 0 indexed
+            }
+        }
+    }
+
+    public function part2()
+    {
+        $input = $this->getInputAsString();
+        for ($i = 13; $i < strlen($input); $i++) {
+            if ($this->noDuplicateCharacters(substr($input, $i-13, 14))) {
+                return $i + 1; // Elves use 1-indexed arrays, not 0 indexed
+            }
+        }
+    }
+
+    private function noDuplicateCharacters(string $str)
+    {
+        $characters = collect(str_split($str));
+        return $characters->duplicates()->isEmpty();
+    }
+}
